e82afa310d838fcf1d6397c952a8cc2233dda3fe
Shao Jiayi <1069607421@qq.com>
2023-12-09 10:52:42 +0800

    增加链表的定义

--- leetcode/two_number_add/two_number_add/two_number_add.cpp
+++ leetcode/two_number_add/two_number_add/two_number_add.cpp
@@ -1,2 +1,12 @@
 #define  _CRT_SECURE_NO_WARNINGS
-#include
+#include<iostream>
+
+  //Definition for singly-linked list.
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+ 
