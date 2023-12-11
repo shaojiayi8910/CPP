bfac918292e20075675e6945b841d43cd80ae77b
Shao Jiayi <1069607421@qq.com>
2023-12-11 23:47:35 +0800

    #lc02 目前来说还少main中的链表的生成与实现

--- leetcode/two_number_add/two_number_add/two_number_add.cpp
+++ leetcode/two_number_add/two_number_add/two_number_add.cpp
@@ -1,5 +1,7 @@
 #define  _CRT_SECURE_NO_WARNINGS
 #include<iostream>
+#include<new>
+using namespace std;
 
   //Definition for singly-linked list.
   struct ListNode {
@@ -9,4 +11,47 @@
       ListNode(int x) : val(x), next(nullptr) {}
       ListNode(int x, ListNode *next) : val(x), next(next) {}
   };
- 
+  class Solution {
+  public:
+      ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+          int total = 0;//记录总值
+          int next = 0;//进位
+          ListNode * result = new ListNode();//listnode的结果
+          ListNode * cur = result;//当前的listnode
+          while (l1 != NULL && l2 != NULL)
+          {
+              total = l1->val + l2->val + next;
+              cur->next = new ListNode(total % 10);
+              next = total / 10;
+              l1 = l1->next;
+              l2 = l2->next;
+              cur = cur->next;
+           }
+          while (l1 != NULL)
+          {
+              total = l1->val + next;
+              cur->next = new ListNode(total % 10);
+              next = total / 10;
+              l1 = l1->next;
+              cur = cur->next;
+          }
+          while (l2 != NULL)
+          {
+              total = l2->val + next;
+              cur->next = new ListNode(total % 10);
+              next = total / 10;
+              l2 = l2->next;
+              cur = cur->next;
+          }
+          if (next != 0)
+          {
+              cur->next = new ListNode(next);
+          }
+          return result->next;
+      }
+  };
+  int main()
+  {   
+      ListNode;
+      return 0;
+  }
