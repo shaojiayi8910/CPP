d2b12c2dbda50fe31c859b76b4dbafc08b778719
Shao Jiayi <1069607421@qq.com>
2023-12-12 23:27:43 +0800

    在main函数中实现两个链表，并调用solution实现在控制台中的输出

--- leetcode/two_number_add/two_number_add/two_number_add.cpp
+++ leetcode/two_number_add/two_number_add/two_number_add.cpp
@@ -51,7 +51,48 @@ using namespace std;
       }
   };
   int main()
-  {   
-      ListNode;
+  {   //第一个链表的实现
+      ListNode* head = new ListNode(4);
+      ListNode* node2 = new ListNode(2);
+      ListNode* node3 = new ListNode(3);
+      ListNode* node4 = new ListNode(1);
+      //开辟各个节点的空间
+      head->next = node2;
+      node2->next = node3;
+      node3->next = node4;
+      node4->next = nullptr;
+      //实现各个节点的连接关系
+      ListNode * cur = head; //用一个指针cur来指向当前结点，初始为头结点
+      while (cur != nullptr) { //当cur不为空时，循环继续
+          cout << cur->val << " "; //打印出当前结点的值
+          cur = cur->next; //将cur指向下一个结点
+      }
+      cout << endl; //换行
+      ///////////////////////////////////////////
+       //第二个链表的实现
+      ListNode* head1  = new ListNode(8);
+      ListNode* node21 = new ListNode(5);
+      ListNode* node31 = new ListNode(3);
+      ListNode* node41  = new ListNode(7);
+      //开辟各个节点的空间
+      head1->next = node21 ;
+      node21->next = node31 ;
+      node31->next = node41 ;
+      node41->next = nullptr;
+      //实现各个节点的连接关系
+      ListNode* cur1 = head1; //用一个指针cur来指向当前结点，初始为头结点
+      while (cur1 != nullptr) { //当cur不为空时，循环继续
+          cout << cur1->val << " "; //打印出当前结点的值
+          cur1 = cur1->next; //将cur指向下一个结点
+      }
+      cout << endl; //换行
+      cout << "after add:" << endl;
+      Solution s;
+      ListNode* _res = s.addTwoNumbers(head, head1);
+      while ( _res!= nullptr)
+      {
+          cout << _res->val << " ";
+          _res = _res->next;
+      }
       return 0;
   }
