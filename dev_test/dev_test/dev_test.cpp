9f1d286ef25379f788f74b718d4bfd69fee2b097
Shao Jiayi <1069607421@qq.com>
2024-02-08 13:15:32 +0800

    dev分支第一次修改提交

---
+++ dev_test/dev_test/dev_test.cpp
@@ -0,0 +1,10 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "´´½¨mainº¯Êý" << endl;
+	cout << "dev branch NO 1 commit" << endl;
+
+	return 0;
+}
